d1bbdf64575111b819f04724d5db90e89749ed95
maisa <maisacavalcanti.santos@gmail.com>
2017-05-20 17:05:15 -0300

    adicionando views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.conf.urls import include, url
+from . import views
+urlpatterns = [
+    url(r'^$', views.post_list),
+]
 # Create your views here.
